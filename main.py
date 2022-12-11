955ca8b3a08494a76b57125a0b2e417ebd9ba913
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2022-12-11 22:00:21 +0100

    adding comments

--- main.py
+++ main.py
@@ -1,70 +1,93 @@
+#### this dictionary has operators as key and their translation into words as value pair
 operators = {
-  "+": "plus",
-  "-": "minus",
-  "*": "multiply by",
-  "/": "divided by",
-  "=": "equals to"
-}
-numbers = {
-  1: 'One',
-  2: 'Two',
-  3: 'Three',
-  4: 'Four',
-  5: 'Five',
-  6: 'Six',
-  7: 'Seven',
-  8: 'Eight',
-  9: 'Nine',
-  10: 'Ten',
-  11: 'Eleven',
-  12: 'Twelve',
-  13: 'Thirteen',
-  14: 'Fourteen',
-  15: 'Fifteen',
-  16: 'Sixteen',
-  17: 'Seventeen',
-  18: 'Eighteen',
-  19: 'Nineteen',
-  20: 'Twenty',
-  30: 'Thirty',
-  40: 'Forty',
-  50: 'Fifty',
-  60: 'Sixty',
-  70: 'Seventy',
-  80: 'Eighty',
-  90: 'Ninety',
-  0: 'Zero'
+    "+": "plus",
+    "-": "minus",
+    "*": "multiply by",
+    "/": "divided by",
+    "=": "equals to"
 }
 
+##### this numbers array has digits as key and translation into words a value pair
+numbers = {
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen',
+    14: 'Fourteen',
+    15: 'Fifteen',
+    16: 'Sixteen',
+    17: 'Seventeen',
+    18: 'Eighteen',
+    19: 'Nineteen',
+    20: 'Twenty',
+    30: 'Thirty',
+    40: 'Forty',
+    50: 'Fifty',
+    60: 'Sixty',
+    70: 'Seventy',
+    80: 'Eighty',
+    90: 'Ninety',
+    0: 'Zero'
+}
+
+
+#### this function is used to convert any number between 0-90 into words,
 
 def convertNumber2Word(n):
-  n = int(n)
-  try:
-    return ""+numbers[n]
-  except KeyError:
+    n = int(n)
     try:
-      return ""+numbers[n - n % 10] + numbers[n % 10].lower()
+        return ""+numbers[n]
     except KeyError:
-      print('Number out of range')
+        try:
+            return ""+numbers[n - n % 10] + numbers[n % 10].lower()
+        except KeyError:
+            return False
 
 
-def solveProblem():
-  result = ''
-  formula=input("Please enter your math formula to solve?")
-  answer=eval(formula)
-  for i in formula:
-    if i.isdigit() and i is not None:
-      result=result+convertNumber2Word(i)+" "
-    elif i == "(":
-      result=result+"("+" "
-    elif i==")":
-      result=result+")"+ " "
-    else:
-      result = result + operators[i]+" "
-  print(result+" "+operators["="]+" "+str(convertNumber2Word(answer)))
-  
+# In this method we are supposed to take an input math formula based on numbers as a string from user between
+# 0-9, and operators as given in operators dictionary above, and then we are looping over the string formula,
+# and checking if it's digit operator or small brackets, based on each condition we are saving result into output variable,
+#while parsing numbers into  above function convertNumber2Word to get translation, 
+# and getting repsective operators with index
 
-solveProblem()
+def main():
+    output = ''
+    result = ""
+
+    try:
+        ### getting input from user i.e 2+3*(2+4)-4/2 as a string
+        formula = input("Please enter your math formula to solve?")
+        #### using eval built-in method to take math formula as a string and give an output as a final result
+        answer = eval(formula)
+        ### converting the answer number into words
+        result = convertNumber2Word(answer)
+        ### using loop to iterate over each number into string formula
+        for i in formula:
+            if i.isdigit() and i is not None:
+                output = output+convertNumber2Word(i)+" "
+            elif i == "(":
+                output = output+"("+" "
+            elif i == ")":
+                output = output+")" + " "
+            else:
+                output = output + operators[i]+" "
+        if not result:
+            print('Number out of range')
+        else:
+
+            print(output+" "+operators["="]+" "+str(result))
+    except Exception as e:
+        print(e)
 
 
-
+#### calling the main method ############
+main()
